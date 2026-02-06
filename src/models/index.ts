d34d6d0e01dde3b7d4a0d12f508e6b9ce3ee2ed5
Kirill Sidora <kirillsidora@yandex.ru>
2026-02-06 21:36:10 +0300

    [SO-66, SO-67, SO-68] feat: Added logicaly link

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,8 @@
 import { initThreadStatisticModel } from "@models/ThreadStatistic";
-import { initCommentModel } from "@models/Comment";
+import { initCommentModel, Comment } from "@models/Comment";
 import { dataBaseConfig } from "@utils/configs";
 import { initTopicModel } from "@models/Topic";
-import { initUserModel } from "@models/User";
+import { initUserModel, User } from "@models/User";
 import { Sequelize } from "sequelize";
 
 const initializeModels = (): void => {
@@ -10,6 +10,9 @@ const initializeModels = (): void => {
     initTopicModel(sequelize);
     initCommentModel(sequelize);
     initThreadStatisticModel(sequelize);
+
+    Comment.hasMany(User, { foreignKey: "id", as: "user"});
+    User.belongsTo(Comment, { foreignKey: "id", as: "comments"});
 }
 
 export const sequelize = new Sequelize(
